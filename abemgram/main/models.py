f50427eebbd2506323506b12cbae39ff29f196fd
Abemelech <abemelech.mesfin@gmail.com>
2022-05-10 19:26:48 -0500

    first db model

--- abemgram/main/models.py
+++ abemgram/main/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="message_sender")
+    sent_to = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="message_reciever", null=True)
+    message_text = models.TextField(max_length=4000)
+    timestamp = models.DateTimeField("time sent")
+    
+    def __str__(self):
+        return self.message_text
