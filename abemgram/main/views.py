ef36c2dc78d50a1444c55038a72149fafcb1da1d
Abemelech <abemelech.mesfin@gmail.com>
2022-05-12 00:48:14 -0500

    extended the userform, deleted database, and some unresolve issues with phone extension, before recreating db

--- abemgram/main/views.py
+++ abemgram/main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Message
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(response):
-    return render(response, 'main/home.html', {})
+    if response.method == "POST":
+        
+        search = response.POST.get('search_number')
+        users = User.objects.filter(phone = search)
+
+        return render(response, 'main/home.html', {"users": users, "search": search})
+    else:
+        return render(response, 'main/home.html', {})
+
+    
--- abemgram/register/views.py
+++ abemgram/register/views.py
@@ -6,7 +6,7 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             return redirect("/login")
 
     else:
