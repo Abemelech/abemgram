9e6883d7207a4dc8401846f8c0e569eecd3b8989
Abemelech <abemelech.mesfin@gmail.com>
2022-05-11 20:35:55 -0500

    form crispyfied

--- abemgram/abemgram/settings.py
+++ abemgram/abemgram/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
+    'crispy_forms',
     'main.apps.MainConfig',
     'register.apps.RegisterConfig',
 ]
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
