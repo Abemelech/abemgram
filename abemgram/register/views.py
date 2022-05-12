d4e635adc58d1b0ad200b72d0b7dd87c0b750f96
Abemelech <abemelech.mesfin@gmail.com>
2022-05-11 20:56:04 -0500

    minor leftover change

--- abemgram/register/views.py
+++ abemgram/register/views.py
@@ -1,4 +1,3 @@
-from re import L
 from django.shortcuts import redirect, render
 from .forms import RegisterForm
 
@@ -8,7 +7,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("/login")
 
     else:
         form = RegisterForm()
