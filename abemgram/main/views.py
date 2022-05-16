c84c8ef3e861ef099557662b09d1ae657ff23cdc
Abemelech <abemelech.mesfin@gmail.com>
2022-05-16 15:31:56 -0500

    fix alternative chat using message query

--- abemgram/main/views.py
+++ abemgram/main/views.py
@@ -1,6 +1,8 @@
+from email import message
 from django.shortcuts import render
 from .models import Message, Customer
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 # Create your views here.
 def index(response):
@@ -18,12 +20,15 @@ def chat(response, number):
     # If chat has been selected a specific number
     username = Customer.objects.get(phone = number).user
     chat_user = User.objects.get(username = username)
+
+    messages = Message.objects.filter(Q(user=response.user) | Q(user=chat_user), Q(sent_to=response.user) | Q(sent_to=chat_user)).order_by("timestamp")
+
     # Similar Search function in index
     if response.method == "POST":
         
         search = response.POST.get('search_number')
         chats = Customer.objects.filter(phone = search)
 
-        return render(response, 'main/home.html', {"chats": chats, "search": search, "chat_number": number, 'chat_user': chat_user})
+        return render(response, 'main/home.html', {"chats": chats, "search": search, "chat_number": number, 'chat_user': chat_user, 'messages': messages})
     else:
-        return render(response, 'main/home.html', {"chat_number": number, 'chat_user': chat_user})
+        return render(response, 'main/home.html', {"chat_number": number, 'chat_user': chat_user, 'messages': messages})
